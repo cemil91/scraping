6e16d6391bb6b04791e4cc35548729146719da8b
Jamil <cemil_mm@mail.ru>
2024-12-24 13:04:23 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+const PORT = 2000;
+
+async function getPageSource(url) {
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+  try {
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const pageSource = await page.content();
+    await browser.close();
+    return pageSource;
+  } catch (error) {
+    await browser.close();
+    throw error;
+  }
+}
+
+
+app.get('/get-page-source', async (req, res) => {
+  const { url } = req.query; 
+  if (!url) {
+    return res.status(400).json({ error: 'URL error' });
+  }
+
+  try {
+    const source = await getPageSource(url);
+    res.status(200).send(source); 
+  } catch (error) {
+    res.status(500).json({ error: 'error', details: error.message });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started http://localhost:${PORT}`);
+});
