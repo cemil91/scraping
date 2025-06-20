706360b48b301270a0a5caac79ef0fa454915561
Camil Malikov <cemil_mm@mail.ru>
2025-06-20 13:25:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
-//const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
 const app = express();
 const PORT = process.env.PORT || 2000;
 
-// async function getPageSource(url) {
-//   const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-//   try {
-//     const page = await browser.newPage();
-//     await page.goto(url, { waitUntil: 'networkidle2' });
-//     const pageSource = await page.content();
-//     await browser.close();
-//     return pageSource;
-//   } catch (error) {
-//     await browser.close();
-//     throw error;
-//   }
-// }
+async function getPageSource(url) {
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+  try {
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const pageSource = await page.content();
+    await browser.close();
+    return pageSource;
+  } catch (error) {
+    await browser.close();
+    throw error;
+  }
+}
 
 app.get('/', async (req, res) => {
 
@@ -24,19 +24,19 @@ app.get('/', async (req, res) => {
 
 
 });
-// app.get('/get-page-source', async (req, res) => {
-//   const { url } = req.query; 
-//   if (!url) {
-//     return res.status(400).json({ error: 'URL error' });
-//   }
+app.get('/get-page-source', async (req, res) => {
+  const { url } = req.query; 
+  if (!url) {
+    return res.status(400).json({ error: 'URL error' });
+  }
 
-//   try {
-//     const source = await getPageSource(url);
-//     res.status(200).send(source); 
-//   } catch (error) {
-//     res.status(500).json({ error: 'error', details: error.message });
-//   }
-// });
+  try {
+    const source = await getPageSource(url);
+    res.status(200).send(source); 
+  } catch (error) {
+    res.status(500).json({ error: 'error', details: error.message });
+  }
+});
 
 
 app.listen(PORT, '0.0.0.0', () => {
