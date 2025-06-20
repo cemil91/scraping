8cdde92fe5bddc1d3ced27c5ea95ba49825e3a67
Camil Malikov <cemil_mm@mail.ru>
2025-06-20 13:41:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,11 @@
-// const express = require('express');
-// const puppeteer = require('puppeteer');
-
-// const app = express();
-// const PORT = process.env.PORT || 2000;
-
-// async function getPageSource(url) {
-//   const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-//   try {
-//     const page = await browser.newPage();
-//     await page.goto(url, { waitUntil: 'networkidle2' });
-//     const pageSource = await page.content();
-//     await browser.close();
-//     return pageSource;
-//   } catch (error) {
-//     await browser.close();
-//     throw error;
-//   }
-// }
-
-// app.get('/', async (req, res) => {
-
-//     return res.status(200).json({ success: 'test' });
-
-
-// });
-// app.get('/get-page-source', async (req, res) => {
-//   const { url } = req.query; 
-//   if (!url) {
-//     return res.status(400).json({ error: 'URL error' });
-//   }
-
-//   try {
-//     const source = await getPageSource(url);
-//     res.status(200).send(source); 
-//   } catch (error) {
-//     res.status(500).json({ error: 'error', details: error.message });
-//   }
-// });
-
-
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server started http://localhost:${PORT}`);
-// });
-
 const express = require('express');
-const puppeteer = require('puppeteer-core');
-const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = process.env.PORT || 2000;
+const PORT = 2000;
 
 async function getPageSource(url) {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath || '/usr/bin/google-chrome-stable',
-    headless: chromium.headless,
-    defaultViewport: chromium.defaultViewport,
-  });
-
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
   try {
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
@@ -70,25 +18,22 @@ async function getPageSource(url) {
   }
 }
 
-app.get('/', async (req, res) => {
-  return res.status(200).json({ success: 'test' });
-});
 
 app.get('/get-page-source', async (req, res) => {
-  const { url } = req.query;
+  const { url } = req.query; 
   if (!url) {
     return res.status(400).json({ error: 'URL error' });
   }
 
   try {
     const source = await getPageSource(url);
-    res.status(200).send(source);
+    res.status(200).send(source); 
   } catch (error) {
     res.status(500).json({ error: 'error', details: error.message });
   }
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+
+app.listen(PORT, () => {
   console.log(`Server started http://localhost:${PORT}`);
 });
-
